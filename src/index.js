b13d37b6629f1395f1c0a7371f8e36b8cd1de106
platisam <platisam@gmail.com>
2023-06-15 18:03:38 +0200

    setting classes conditionally

--- src/index.js
+++ src/index.js
@@ -109,7 +109,7 @@ function Pizza({ pizzaObj }) {
   //if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
